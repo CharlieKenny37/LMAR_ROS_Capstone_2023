5c1d4e819f1c7ff8d1f56b3bcb9dcae0a1e9c902
ryan-donald <bluefalcon0990@gmail.com>
2023-04-28 19:56:21 -0400

    motor controller via GPIO updated

--- src/lmar_ros/src/motor_gpio.py
+++ src/lmar_ros/src/motor_gpio.py
@@ -10,12 +10,7 @@ class motor_gpio():
     
     def __init__(self):
         rospy.init_node("motor_controller")
-        #self.last_time = rospy.time()
-        #self.pins = [32, 33]
-        #self.dir_pins = []
         GPIO.setmode(GPIO.BOARD)
-        #for x in self.pins:
-        #    GPIO.setup(x, GPIO.OUT)
 
         #RIGHT SIDE
         self.pwm_right_pin = 32
@@ -25,6 +20,8 @@ class motor_gpio():
         self.pwm_right = GPIO.PWM(self.pwm_right_pin,4000)
         self.pwm_right.start(0)
         GPIO.output(self.right_dir_pin,0)
+
+        
         #LEFT SIDE
         self.pwm_left_pin = 33
         self.left_dir_pin = 31
@@ -38,83 +35,62 @@ class motor_gpio():
         #GPIO.output(36,1)
         rospy.Subscriber('cmd_vel', Twist ,self.motor_callback)
         
-        #pwm = GPIO.PWM(32, 20)
-        #pwm.start(0)
-        #pwm.ChangeDutyCycle(100)
         while(not rospy.is_shutdown()):
-            ""
-            #pwm.ChangeDutyCycle(100)
-            #if(rospy.time() - self.last_time < 1):
-            #    for x in np.size(self.pins):
-            #        GPIO.output(self.pins[x], self.last_msg)
-            #        GPIO.outpit(self.dir_pins[x], 1)
-            #else:
-            #    for x in self.pins:
-            #        GPIO.output(self.pins[x], 0)
-            #        GPIO.outpit(self.dir_pins[x], 0)
-        
-        #pwm.ChangeDutyCycle(0)
-        #GPIO.output(36,0)
+            pass
         GPIO.cleanup()
-        """
-        while(not rospy.is_shutdown):
-            if(rospy.time() - self.last_time < 1):
-                for x in np.size(self.pins):
-                    if(self.last_msg.twist.linear.x > 0.1:
-                        GPIO.output(self.pins[x], self.last_msg)
-                        GPIO.outpit(self.dir_pins[x], 1)
-                    elif(np.abs(self.last_msg.twist.angular.z) > 0.1):
-                        GPIO.output(self.pins[x], self.last_msg)
-                        GPIO.outpit(self.dir_pins[x], x)
-            else:
-                for x in self.pins:
-                    GPIO.output(self.pins[x], 0)
-                    GPIO.outpit(self.dir_pins[x], 0)
-        """
-
 
 
     def motor_callback(self, msg):
-        # self.last_time = rospy.time()
-        # self.last_msg = 255
-        rospy.logerr("Linear X:")
-        rospy.logerr(msg.linear.x)
-        rospy.logerr("Angular Z:")
-        rospy.logerr(msg.angular.z)
-        if(msg.linear.x > 0.3):
+
+        #clips the speed if it is over 1.0, [0.0, 1.0] is the range, as a percentage of maximum speed
+        if(msg.linear.x > 1.0):
+            msg.linear.x = 1.0
+
+        elif(msg.linear.x < -1.0):
+            msg.linear.x = -1.0
+        
+        elif(msg.angular.z > 1.0):
+            msg.angular.z = 1.0
+        
+        elif(msg.angular.z < -1.0):
+            msg.angular.z = -1.0
+
+
+        #determines the value in the message to send to each pwm pi, and what value to send to each direction pin
+        if(msg.linear.x > 0.0):
             #right side
-            self.pwm_right.ChangeDutyCycle(100)
+            self.pwm_right.ChangeDutyCycle(100*np.abs(msg.linear.x))
             GPIO.output(self.right_dir_pin, 1)
 
             #left side
-            self.pwm_left.ChangeDutyCycle(100)
+            self.pwm_left.ChangeDutyCycle(100*np.abs(msg.linear.x))
             GPIO.output(self.left_dir_pin, 0)
 
-        elif(msg.linear.x < -0.3):
+        elif(msg.linear.x < 0.0):
             #right side
-            self.pwm_right.ChangeDutyCycle(100)
+            self.pwm_right.ChangeDutyCycle(100*np.abs(msg.linear.x))
             GPIO.output(self.right_dir_pin, 0)
 
             #left side
-            self.pwm_left.ChangeDutyCycle(100)
+            self.pwm_left.ChangeDutyCycle(100*np.abs(msg.linear.x))
             GPIO.output(self.left_dir_pin, 1)
             
-        elif(msg.angular.z < -0.3):
+        elif(msg.angular.z < 0.0):
             #right side
-            self.pwm_right.ChangeDutyCycle(100)
+            self.pwm_right.ChangeDutyCycle(100*np.abs(msg.anglar.z))
             GPIO.output(self.right_dir_pin, 0)
 
             #left side
-            self.pwm_left.ChangeDutyCycle(100)
+            self.pwm_left.ChangeDutyCycle(100*np.abs(msg.angular.z))
             GPIO.output(self.left_dir_pin, 0)
 
-        elif(msg.angular.z > 0.3):
+        elif(msg.angular.z > 0.0):
             #right side
-            self.pwm_right.ChangeDutyCycle(100)
+            self.pwm_right.ChangeDutyCycle(100*np.abs(msg.angular.z))
             GPIO.output(self.right_dir_pin, 1)
 
             #left side
-            self.pwm_left.ChangeDutyCycle(100)
+            self.pwm_left.ChangeDutyCycle(100*np.abs(msg.angular.z))
             GPIO.output(self.left_dir_pin, 1)
             
         else:
